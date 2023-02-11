42e593c5ad188fe824a882f7e3e9fcaaa3dc7e80
Wesley Maia <maia.weol@gmail.com>
2023-02-11 09:51:57 -0800

    Feature: Information extra JSON

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -5,6 +5,10 @@ class ContactSerializer < ActiveModel::Serializer
   has_many :phones
   has_one :address
 
+  meta do
+    { author: "Jackson Pires" }
+  end
+
   def attributes(*args)
     h = super(*args)
     # pt-BR --->
