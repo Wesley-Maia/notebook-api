33d4d760ce66878c7f5ff53a7fd84ade0f54947b
Wesley Maia <maia.weol@gmail.com>
2023-02-15 14:58:52 -0800

    Feature: relationship belongs_to

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,7 +2,7 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
   belongs_to :kind do
-    link(:related) { kind_url(object.kind.id) }
+    link(:related) { contact_kind_url(object.id) }
   end
   has_many :phones
   has_one :address
